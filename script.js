e774a4ea84c2b1dcd5a4945c2fca3faeab4f49a6
Luis Guilherme <lgsguirardelli@gmail.com>
2023-04-01 10:09:13 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+var img = document.getElementById('picture')
+
+function kendall() {
+    answer.innerHTML = '';
+    img.src = '';
+
+    answer.innerHTML = '<p>Jeremy Strong</p>';
+    img.src = 'jeremy-3.jpg'
+      } 
+
+function logan() {
+    answer.innerHTML = '';
+    img.src = '';
+
+    answer.innerHTML = '<p>Brian Cox</p>'
+    img.src = 'briancox.jpg';
+}
+
+function shiv() {
+    answer.innerHTML = '';
+    img.src = '';
+
+    answer.innerHTML = '<p>Sarah Snook</p>'
+    img.src = 'sarah.jpg';
+}
+
+function roman() {
+    answer.innerHTML = '';
+    img.src = '';
+
+    answer.innerHTML = '<p>Kieran Culkin</p>'
+    img.src = 'kieran.jpg';
+}
+
+function tom() {
+    answer.innerHTML = '';
+    img.src = '';
+
+    answer.innerHTML = '<p>Matthew Macfadyen</p>'
+    img.src = 'tom.jpg';
+}
+
+function connor() {
+    answer.innerHTML = '';
+    img.src = '';
+
+    answer.innerHTML = '<p>Alan Ruck</p>'
+    img.src = 'alan.jpg';
+}
+
+function gregg() {
+    answer.innerHTML = '';
+    img.src = '';
+
+    answer.innerHTML = '<p>Nicholas Braun</p>'
+    img.src = 'nicholas.jpg';
+}
